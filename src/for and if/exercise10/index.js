2001afecd8cc82ce10c1e5b0e11092bffa39079f
bbpardo <b.b.pardo@protonmail.com>
2022-02-21 14:46:47 +0100

    all done

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,3 +1,17 @@
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
 
-// Put your code here
+for (let currentPosition = 0; currentPosition < numbers.length ; currentPosition++) {
+    for (
+        let compareWithPosition = currentPosition+1;
+        compareWithPosition < numbers.length;
+        compareWithPosition++
+    ){
+        if (numbers[currentPosition] > numbers[compareWithPosition]) {
+            let temp = null;
+            temp = numbers[currentPosition]
+            numbers[currentPosition] = numbers[compareWithPosition];
+            numbers[compareWithPosition] = temp;
+        }
+    }
+}
+console.log(numbers)
