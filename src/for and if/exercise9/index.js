cfe0119c3168a04f37e6872a91e5c450d537e53b
bbpardo <b.b.pardo@protonmail.com>
2022-02-22 13:16:14 +0100

    exercise 9 diferent

--- src/for and if/exercise9/index.js
+++ src/for and if/exercise9/index.js
@@ -1,7 +1,11 @@
-let numbers = [0,1,2,3,4,5,6,7,8,9]
-
-for ( let left of numbers) {
-    for (let right of numbers) {
-        console.log(left*right)
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+for(let left of numbers){
+    if(left>0){
+        console.log("\nTabla del " + left +"\n")
+        for(let right of numbers){
+            if(right>0){
+                console.log( `${left} x ${right} = ${left*right}`);
+            }
+        }
     }
 }
