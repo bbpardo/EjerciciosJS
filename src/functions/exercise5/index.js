2f96acfef9f33d41241d8038516422ffe223c740
bbpardo <b.b.pardo@protonmail.com>
2022-02-23 13:07:26 +0100

    exercise 5 done

--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -1,7 +1,14 @@
 const numbers = [11,9,13,12]
 
-// Put your code here
-
+function minorInRange(a,b,array){
+    let minor = array[a]
+    for(let idx = a; idx <= b;idx++){
+        if(array[idx] < minor){
+            minor = array[idx]
+        }
+    }
+    return minor
+}
 console.log(minorInRange(0,3,numbers));
 console.log(minorInRange(2,3,numbers));
-console.log(minorInRange(1,3),numbers);
+console.log(minorInRange(1,3,numbers));
