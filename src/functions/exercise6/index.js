d59e9d381d3d5d279aeecf9b810656ee2c9864fc
bbpardo <b.b.pardo@protonmail.com>
2022-02-23 13:18:15 +0100

    exercise 6 done

--- src/functions/exercise6/index.js
+++ src/functions/exercise6/index.js
@@ -1,7 +1,17 @@
 const numbers = [11,9,13,12]
 
-// Paste your functions here
-
-// Put your code here
+function orderRange(a, b ,array){
+    let temp = array[a]
+    for(let outer = a ; outer <= b; outer++){
+        for(let inner= a; inner <= b; inner++){
+            if(numbers[outer] < numbers[inner]){
+                temp = numbers[outer];
+                numbers[outer] = numbers[inner];
+                numbers[inner] = temp;         
+            }
+        }
+    }
+}
+orderRange(0,2,numbers)
 
 console.log(numbers);
