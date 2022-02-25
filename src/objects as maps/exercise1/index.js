7a9c916bbf14ca93b682f1175df3c287f6a1db0d
bbpardo <b.b.pardo@protonmail.com>
2022-02-25 14:30:50 +0100

    Exercise object done

--- src/objects as maps/exercise1/index.js
+++ src/objects as maps/exercise1/index.js
@@ -1,14 +1,15 @@
 const phrase = "No hay bien que por mal no venga";
 const counters = {};
 function countLetter (letter, countersObject) {
-    
-
+    if(counters.hasOwnProperty(letter) === false){
+        counters[letter] = 0;
+    }
+    if(counters.hasOwnProperty(letter)){
+        counters[letter]++;
+    }
 }
-
-
 for (letter of phrase) {
     console.log(letter)
     countLetter(letter, counters)
 }
-
-console.log(counters)
+console.log(JSON.stringify(counters))
