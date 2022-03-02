96c06f82217b5887e83fc72778388cb3c9641e41
bbpardo <b.b.pardo@protonmail.com>
2022-03-02 14:04:43 +0100

    exercise8 done

--- src/functions/exercise8/index.js
+++ src/functions/exercise8/index.js
@@ -1,26 +1,33 @@
 numbers = [4,2,9,6,1,0,3,6,5,2,9];
 
 function aGreatherThanB (a, b) {
-    // Put your code here
+    return numbers[a]>numbers[b]
 }
 
 function bGreatherThanA (a,b) {
-    // Put your code here
+    return numbers[a]<numbers[b]
 }
 
 function swap (idxA, idxB, array) {
-    // Put your code here
+    if(true){
+        let swapVar = null;
+        swapVar = array[idxA]
+        array[idxA] = array[idxB]
+        array[idxB] = swapVar
+    }
 }
 
 function sort (comparator, array) {
-    for ( let currentIdx = 0 ; currentIdx < array.lenght-1 ; currentIdx++) {
-        for ( let nextIdx = currentIdx+1 ; nextIdx < array.lenght ; nextIdx++ ) {
+    for ( let currentIdx = 0 ; currentIdx < array.length-1 ; currentIdx++) {
+        for ( let nextIdx = currentIdx+1 ; nextIdx < array.length ; nextIdx++ ) {
             if (
-                comparator(array.currentIdx, array.nextIdx)
-            ) swap (array.currentIdx, array.nextIdx, array)
+                comparator(currentIdx, nextIdx)
+            ) swap (currentIdx, nextIdx, array)
         }
     }
 }
 
 sort(aGreatherThanB, numbers)
+console.log(numbers)
 sort(bGreatherThanA, numbers)
+console.log(numbers)
