16f9f80d9c150885376d78f8117e52ea059c9729
bbpardo <b.b.pardo@protonmail.com>
2022-03-02 09:59:14 +0100

    Pequeño cambio en exercise 10

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -4,7 +4,7 @@ for(let outer = 0; outer < numbers.length; outer++){
     for(let inner = outer + 1; inner < numbers.length; inner++
     ){
         if(numbers[outer] > numbers[inner]){
-            let temp = null;
+            let temp = null;gi
             temp = numbers[outer];
             numbers[outer] = numbers[inner];
             numbers[inner] = temp;
