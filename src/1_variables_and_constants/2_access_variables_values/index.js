c15a7ec6de7ec10aeda11628f0e427b19f0c97f8
DanBaDo <byte4life@pm.me>
2023-01-30 18:30:03 +0100

    Deletes solution in access variables exercise

--- src/1_variables_and_constants/2_access_variables_values/index.js
+++ src/1_variables_and_constants/2_access_variables_values/index.js
@@ -2,7 +2,7 @@
  * You can use a variable name in place his value, any way.
  * 
  * Exercice:
- * - Declare a new variable named "myVariable" and assign they 42 value
+ * - Declare a new variable named "myVariable" and assign the 42 value to it
  * using "secondVariable" for this.
  */
 
@@ -10,7 +10,6 @@ let firstVariable = 42
 let secondVariable = firstVariable // secondVariable value is 42
 
 // Write your code here.
-let myVariable = secondVariable
 
 /**
  * Don't to change next lines.
