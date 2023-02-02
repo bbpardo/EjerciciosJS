1d67861b6883dc94160d5bd3185aff2f131bf009
DanBaDo <byte4life@pm.me>
2023-02-02 17:35:48 +0100

    Renames folder

--- src/4_complex_types/index.js
+++ src/4_complex_types/index.js
@@ -1,16 +1,16 @@
 /**
  * There are some kinds of "data" types in JavaScript composed
  * by smaller data pieces.
- * - arrays - are ordered lists of values of any kind represented by [],
- * like ["apple", "orange", "banana"]
+ * - arrays - are ordered lists of values of any kind represented by [], like
+ *     ["apple", "orange", "banana"]
  * - objects - are values grouped by lavels or names called "properties", represented by {}, like
- * {
- *   name: "Daniel"
- *   age: 42
- *   enrolled: true
- * }
+ *     {
+ *       name: "Daniel"
+ *       age: 42
+ *       enrolled: true
+ *     }
  * 
- * Exercice:
+ * Exercise:
  * - Declare a variable named "odd" and assign to it
  * a array containin the four first odd numbers.
  * - Declare a variable named "pet" and assign to it an
