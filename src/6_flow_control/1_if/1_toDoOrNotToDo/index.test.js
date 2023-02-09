bba35b280936caad9c3d19d9efd63240737cf8d3
DanBaDo <byte4life@pm.me>
2023-02-09 19:06:32 +0100

    Simplifies exercise

--- src/6_flow_control/1_if/1_toDoOrNotToDo/index.test.js
+++ src/6_flow_control/1_if/1_toDoOrNotToDo/index.test.js
@@ -1,11 +1,5 @@
 const { divisionDe } = require("./index")
 
-test("Avoid to work when not numbers",()=>{
-    expect(divisionDe(1,"")).toBeUndefined()
-    expect(divisionDe("",1)).toBeUndefined()
-    expect(divisionDe("","")).toBeUndefined()
-})
-
 test("Avoid to divide by zero",()=>{
     expect(divisionDe(1,0)).toBeUndefined()
 })
